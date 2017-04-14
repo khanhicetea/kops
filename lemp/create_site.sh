5ea9cf1b2cc88691a2e41010462bc03e8c0cffe3
KhanhIceTea <khanhicetea@gmail.com>
2017-04-14 21:38:45 +0700

    well-known alias

--- lemp/create_site.sh
+++ lemp/create_site.sh
@@ -24,7 +24,7 @@ server {
         server_name ${DOMAIN};
 
         location ^~ /.well-known/acme-challenge/ {
-            alias /usr/share/nginx/acme-challenge/;
+            alias /usr/share/nginx/acme-challenge/.well-known/acme-challenge/;
         }
 
         return 301 https://\$server_name\$request_uri;
