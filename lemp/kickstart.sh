058a44aba951610e58ef2060c2d7f1a5a6cc31c9
KhanhIceTea <khanhicetea@gmail.com>
2017-04-14 22:51:57 +0700

    remove nginx comments

--- lemp/kickstart.sh
+++ lemp/kickstart.sh
@@ -54,14 +54,9 @@ gzip_types text/plain text/css application/json application/x-javascript text/xm
 EOF
 sudo mv /tmp/nginx_conf /etc/nginx/conf.d/nginx.conf
 cat >/tmp/nginx_fastcgi_snipets <<EOF
-# regex to split \$uri to \$fastcgi_script_name and \$fastcgi_path
 fastcgi_split_path_info ^(.+\\.php)(/.+)$;
-
-# Check that the PHP script exists before passing it
 try_files \$fastcgi_script_name =404;
 
-# Bypass the fact that try_files resets \$fastcgi_path_info
-# see: http://trac.nginx.org/nginx/ticket/321
 set \$path_info \$fastcgi_path_info;
 fastcgi_param PATH_INFO \$path_info;
 fastcgi_param HTTP_PROXY "";
