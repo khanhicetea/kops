d8c97dfb4594ed081125319680dde6ac3bd7e449
KhanhIceTea <khanhicetea@gmail.com>
2017-09-14 15:55:41 +0700

    Added application/javascript to gzip_types

--- lemp/kickstart.sh
+++ lemp/kickstart.sh
@@ -120,7 +120,7 @@ gzip_proxied any;
 gzip_comp_level 6;
 gzip_buffers 16 8k;
 gzip_http_version 1.1;
-gzip_types text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript;
+gzip_types text/plain text/css application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript application/javascript;
 EOF
 sudo mv /tmp/nginx_conf /etc/nginx/conf.d/nginx.conf
 cat >/tmp/nginx_fastcgi_snipets <<EOF
