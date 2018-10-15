dedaaf515a0957f6b0f630271a150b6f4fc5d4fc
KhanhIceTea <khanhicetea@gmail.com>
2018-10-15 18:14:35 +0700

    Create mysql-hourly.sh

---
+++ backup/mysql-hourly.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "Please input backup dir : [backup_dir]"
+    exit 1
+fi
+
+if [ -z "$3" ]
+then
+    BACKUP_DBS=$(echo "show databases" | mysql | grep -Ev "^(Database|mysql|performance_schema|information_schema|sys)$")
+else
+    BACKUP_DBS="$3"
+fi
+
+UPLOAD_DIR=${2:}
+
+CWD=$(dirname $0)
+BACKUP_DIR="$1"
+TODAY=`date +%Y-%m-%d`
+BKTIME=`date +%H_%M`
+RM_DAY=`date --date="14 days ago" +%Y-%m-%d`
+FILENAME=$BKTIME.sql.gz
+
+echo "Backup database in $TODAY ...."
+mysqldump --opt --routines --compact --force --databases ${BACKUP_DBS} | gzip > $BACKUP_DIR/$FILENAME
+
+echo "Uploading to Dropbox ...."
+/bin/bash $CWD/dropbox_uploader.sh upload "/${BACKUP_DIR}/${FILENAME}" "${UPLOAD_DIR}${TODAY}/${FILENAME}"
+/bin/bash $CWD/dropbox_uploader.sh delete "${UPLOAD_DIR}${RM_DAY}"
+
+echo "Cleaning ..."
+rm -f "/$BACKUP_DIR/${FILENAME}"
