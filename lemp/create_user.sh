018768a99dd56c29a863d423395db1c62dd0ffc1
KhanhIceTea <khanhicetea@gmail.com>
2019-02-19 16:13:46 +0700

    umask 027

--- lemp/create_user.sh
+++ lemp/create_user.sh
@@ -5,6 +5,7 @@ USERNAME="$1"
 # Create linux user
 sudo useradd -m -N -g nginx $USERNAME
 sudo chsh -s /bin/bash $USERNAME
+echo "umask 027" >> ~/.bashrc
 
 # Composer install plugins
 sudo runuser -l $USERNAME -c 'composer global require "hirak/prestissimo:^0.3"'
