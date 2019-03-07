19192cc2368a91314ad1733e0681de601a2bcceb
KhanhIceTea <khanhicetea@gmail.com>
2019-03-07 10:59:53 +0700

    Create docker.sh

---
+++ scripts/docker.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt remove docker docker-engine docker.io
+sudo apt update
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt update
+sudo apt install docker-ce
