538263ff68c43e1850315402b5a3d7223d4d2e64
KhanhIceTea <khanhicetea@gmail.com>
2020-11-04 06:40:49 +0700

    Update kickstart.sh

--- lemp/kickstart.sh
+++ lemp/kickstart.sh
@@ -115,10 +115,7 @@ sudo mv /tmp/nginx_conf /etc/nginx/nginx.conf
 cat >/tmp/nginx_conf_0 <<EOF
 default_type application/octet-stream;
 
-log_format   main '$remote_addr - $remote_user [$time_local]  $status '
-    '"$request" $body_bytes_sent "$http_referer" '
-    '"$http_user_agent" "$http_x_forwarded_for"';
-access_log  /var/log/nginx/access.log main;
+access_log  /var/log/nginx/access.log combined;
 
 keepalive_timeout 120;
 keepalive_requests 1024;
